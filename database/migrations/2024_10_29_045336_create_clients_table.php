243f05e1752f0d93650b548ad99282652321c791
molotovisq <josephgdroid@gmail.com>
2024-11-04 01:56:54 -0300

    Update clients table, fk with user

--- database/migrations/2024_10_29_045336_create_clients_table.php
+++ database/migrations/2024_10_29_045336_create_clients_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->unsignedBigInteger('user_id');
             $table->string('nickname');
             $table->text('description');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
