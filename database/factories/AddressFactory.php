b2cdc5847589f397a94c72e0479d558e1717ae54
José Marcolino <jose.silva@seventh.com.br>
2024-11-27 00:17:34 -0300

    Update AddressFactory: regionalizes city, state and neighborhood

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -2,24 +2,40 @@
 
 namespace Database\Factories;
 
+use App\Models\Address;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Address>
- */
 class AddressFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    private function getCityData(): array
     {
         return [
-            'state' => $this->faker->state,
-            'city' => $this->faker->city,
-            'neighborhood' => $this->faker->word,
+            'Teresina' => [
+                'state' => 'PI',
+                'neighborhoods' => [
+                    'Dirceu 1', 'Dirceu 2', 'Promorar', 'Areias', 'Satelite', 'Mocambinho', 'Centro'
+                ]
+            ],
+            'Timon' => [
+                'state' => 'MA',
+                'neighborhoods' => [
+                    'Centro', 'Parque Alvorada', 'Boa Vista', 'Mariza', 'Cidade Nova'
+                ]
+            ]
+        ];
+    }
+
+    public function definition(): array
+    {
+        $cityData = $this->getCityData();
+        $city = $this->faker->randomElement(array_keys($cityData));
+        $state = $cityData[$city]['state'];
+        $neighborhood = $this->faker->randomElement($cityData[$city]['neighborhoods']);
+
+        return [
+            'state' => $state,
+            'city' => $city,
+            'neighborhood' => $neighborhood,
             'street' => $this->faker->streetName,
             'number' => $this->faker->buildingNumber,
             'is_primary' => true,
